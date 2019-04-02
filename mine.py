3012b631616d2cd5547e5090a11f584a5f172875
ZHYHenryZhang <henryzhang19951213@gmail.com>
2019-04-01 23:37:22 -0700

    mine, grid agent class

---
+++ mine.py
@@ -0,0 +1,27 @@
+""" Docstring:
+Description: mine class
+Author: 
+Date:April 01, 2019
+"""
+
+# module
+import numpy as np
+
+# parameters
+
+
+# objects
+class Mine():
+    def __init__(self, level):
+        """ Mine init """
+        self.level = level
+        self.board = None
+
+# functions
+def mine_test():
+    """ testing of mine """
+    pass
+
+# main
+if __name__ == "__main__":
+    mine_test()
